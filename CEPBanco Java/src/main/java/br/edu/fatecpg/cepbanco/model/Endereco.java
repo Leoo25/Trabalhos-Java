58d4f000ea639f985eb9ec6255c402e62ed1df62
Leonardo Mendonça <95057060+Leoo25@users.noreply.github.com>
2025-11-05 02:24:28 -0300

    Add files via upload

---
+++ CEPBanco Java/src/main/java/br/edu/fatecpg/cepbanco/model/Endereco.java
@@ -0,0 +1,87 @@
+package br.edu.fatecpg.cepbanco.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false, length = 9)
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    @Column(length = 2)
+    private String uf;
+
+    public Endereco(Long id, String cep, String logradouro, String numero, String complemento, String uf) {
+        this.id = id;
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.uf = uf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "id=" + id +
+                ", cep='" + cep + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", numero='" + numero + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", uf='" + uf + '\'' +
+                '}';
+    }
+}
